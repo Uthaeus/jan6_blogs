0f1daa8d6a7931a133046640447a626f4dd10013
Uthaeus <Uth@github.com>
2018-01-06 21:37:04 -0700

    Created new blog functionality and show page

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -13,4 +13,22 @@ describe 'navigate' do
     end
   end
 
+  describe 'creation' do
+    before do
+      visit new_blog_path
+    end
+
+    it 'has a form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'blog[title]', with: 'A title'
+      fill_in 'blog[date]', with: Date.today
+      fill_in 'blog[body]', with: "A little bit of content"
+      click_on "Save"
+
+      expect(page).to have_content("A little bit of content")
+    end
+  end
 end
