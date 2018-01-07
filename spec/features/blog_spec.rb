16a9a337a228ba8118b2b3b86f36a6db8f4ce66f
Uthaeus <Uth@github.com>
2018-01-06 18:28:39 -0700

    created Index and new blog pages through testing

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate' do
   describe 'index' do
     it 'can be reached successfully' do
       visit blogs_path
-      expect(blog.status_code).to eq(200)
+      expect(page.status_code).to eq(200)
     end
   end
 end
